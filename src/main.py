24b505285afbf61cf591b62f21f51d2ee359e4de
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2021-12-28 01:14:41 -0300

    Reboot 1

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ Permissions Integer: 292057984064
 
 Enlace para invitar bot: https://discord.com/api/oauth2/authorize?client_id=889312376036425810&permissions=292057984064&scope=bot
 Repositorio: https://github.com/NLGS2907/Alg1-Lector-de-Ejercicios
+
+[12/28/21] Reboot requerido.
 """
 
 from constantes import TOKEN
