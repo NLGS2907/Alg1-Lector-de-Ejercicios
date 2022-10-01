daa3feece9c799e670fa3f092b49954fb22420cf
Franco Lighterman Reismann <flighterman@fi.uba.ar>
2022-10-01 18:57:43 -0300

    reboot 1/10/22

--- src/main/main.py
+++ src/main/main.py
@@ -23,6 +23,7 @@ Repositorio: https://github.com/NLGS2907/Alg1-Lector-de-Ejercicios
 [01/08/22] Reboot Manual Requerido.
 [09/05/22] Reboot Manual.
 [09/29/22] Reboot.
+[10/01/22] Reboot.
 """
 
 from .constantes import TOKEN
